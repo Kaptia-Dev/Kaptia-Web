251303c51d76cf6227bf717e9ec265762f41e0e7
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2026-01-29 17:51:04 -0600

    feat: update Hero, Partners, Product, and Team sections with placeholder content

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,7 @@
 export default function Hero() {
     return (
-        <></>
+        <>
+            El Hero va aquí
+        </>
     );
 }
--- src/sections/Team.jsx
+++ src/sections/Team.jsx
@@ -1,5 +1,7 @@
 export default function Team() {
     return (
-        <></>
+        <>
+            El equipo va aquí
+        </>
     );
 }
