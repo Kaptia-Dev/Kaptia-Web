90448f7256f6ae620a14bf737eb95095dd6e7778
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2026-01-29 17:50:59 -0600

    feat: add Nosotros, Partners, and Producto pages with corresponding metadata and components

---
+++ src/app/partners/page.jsx
@@ -0,0 +1,15 @@
+import EarlyPartners from "@/sections/Partners";
+
+export const metadata = {
+  title: "Early Partners | Kaptia",
+  description:
+    "Únete a nuestros Early Partners y sé parte de la transformación digital en ventas con Kaptia.",
+};
+
+export default function Partners() {
+  return (
+    <main>
+      <EarlyPartners />
+    </main>
+  );
+}
