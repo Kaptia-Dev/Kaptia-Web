e98f09f6672ca83104b410652872b7a273367e68
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2026-01-30 13:44:45 -0600

    feat: add animation prop to NavLink component for customizable animations

--- src/components/ui/NavLink.jsx
+++ src/components/ui/NavLink.jsx
@@ -1,8 +1,13 @@
-export default function NavLink({ text = "", href = "#", idx = 0 }) {
+export default function NavLink({
+  text = "",
+  href = "#",
+  idx = 0,
+  animation = "",
+}) {
   return (
     <li
       style={{ animationDelay: `${idx * 80}ms` }}
-      className="text-white uppercase font-semibold rounded-full animate-slide-up-fade hover:text-kaptia-yellow hover:-translate-y-1 duration-300 transition-all"
+      className={`text-white uppercase font-semibold rounded-full hover:text-kaptia-yellow hover:-translate-y-1 duration-300 transition-all ${animation}`}
     >
       <a href={href}>{text}</a>
     </li>
