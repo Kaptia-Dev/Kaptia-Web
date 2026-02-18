44b073120327d1e5ef493ced232de9c24079b85c
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2026-02-18 15:49:50 -0600

    feat(button): add textColor prop to CustomButton for customizable text color

--- src/components/ui/CustomButton.jsx
+++ src/components/ui/CustomButton.jsx
@@ -3,12 +3,13 @@ export default function CustomButton({
   onClick = () => {},
   primaryColor = "bg-white",
   secondaryColor = "bg-kaptia-yellow",
-  animate = ""
+  animate = "",
+  textColor = "text-kaptia-primary-blue"
 }) {
   return (
     <button
       onClick={onClick}
-      className={`${primaryColor} ${animate} text-kaptia-primary-blue px-12 py-2 text-[1.2rem] font-bold rounded-full relative shadow cursor-pointer duration-300 overflow-hidden group`}
+      className={`${primaryColor} ${animate} ${textColor} px-12 py-2 text-[1.2rem] font-bold rounded-full relative shadow cursor-pointer duration-300 overflow-hidden group`}
     >
       <span
         className={`${secondaryColor} absolute right-full top-0 bottom-0 w-full z-0 group-hover:right-0 transition-all duration-300`}
