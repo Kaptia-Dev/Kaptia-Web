35854680781b2258cc3523e86927a96d70f68c9c
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2026-02-18 11:03:27 -0600

    feat(hero): add sales prospecting statistics section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -47,6 +47,45 @@ export default function Hero() {
         </div>
         <DownButton />
       </section>
+      <section className="h-screen flex flex-col items-center justify-center gap-24 md:gap-16">
+        <div className="flex flex-col justify-center items-center gap-24 md:gap-16">
+          <img
+            className="h-32 animate-slide-in-top"
+            src="./images/figura_7.webp"
+            alt="Figura 7"
+          />
+          <p className="text-3xl md:text-5xl text-white font-extralight text-center w-3/4 md:w-2/4 lg:w-2/3 animate-slide-in-top">
+            La prospección es la{" "}
+            <strong className="font-extrabold text-4xl md:text-6xl">2°</strong>{" "}
+            actividad que{" "}
+            <span className="uppercase font-bold text-secondary-blue-300">
+              más tiempo consume
+            </span>
+            <span className="uppercase font-bold text-yellow-400">
+              {" "}
+              en el proceso de ventas
+            </span>
+          </p>
+        </div>
+        <div className="flex gap-10 justify-center items-center md:w-4/5">
+          <p className="font-extralight text-2xl md:text-4xl lg:text-5xl text-white w-3/4 text-center md:text-start">
+            Menos del{" "}
+            <strong className="font-extrabold text-secondary-blue-300 text-4xl md:text-6xl">
+              2%
+            </strong>{" "}
+            de las empresas productoras en{" "}
+            <span className="font-bold text-yellow-400">México</span>
+            <span className="font-bold text-secondary-blue-300">
+              {" "}
+              utilizan plataformas digitales{" "}
+            </span>
+            <span className="font-bold text-yellow-400">
+              para gestión comercial
+            </span>
+          </p>
+          <img className="h-72 hidden md:flex" src="./images/figura_6.webp" alt="" />
+        </div>
+      </section>
     </>
   );
 }
